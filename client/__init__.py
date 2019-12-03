aacf3e27394d35811af4767cabb6f53952ecb72b
sillywutong <17307130331@fudan.edu.cn>
2019-12-03 23:03:40 +0800

    decode-encoder

--- client/__init__.py
+++ client/__init__.py
@@ -2,12 +2,17 @@ from common.channel.Session import client_new_session
 import socket
 import select
 from client import client_global
-
+import io
 
 def run():
     try:
         client_global.session = client_new_session()
     except ConnectionError:
         print('Server has been closed.')
-    client_global.session.send(msg_type=1, msg_body={'nothing':0})
-    
+    with open('test.png',"rb") as image_file:
+        image = image_file.read()
+        img = bytes(image)
+    #io = io.BytesIO(img)
+    length = client_global.session.send(msg_type=1, msg_body={'nothing':0,'something':[1,2,3,'string!','中文',{'another dict': 12345, 'username':'rarecu'}],'image': img})
+    print("length to send: %d" % length)
+    client_global.session.send(msg_type=2, msg_body={})
--- common/message/__init__.py
+++
@@ -1,5 +0,0 @@
-def pack_app_message(msg_type, msg_body):
-    return b'testing!'
-
-def unpack_message(data):
-    return b'testing!'
